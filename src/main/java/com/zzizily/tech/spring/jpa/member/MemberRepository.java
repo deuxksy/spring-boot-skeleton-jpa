4b3a1dbf5eb8f71f708df499c020e304805ccc55
SeokYoung.Kim <deuxksy@gmail.com>
2019-02-04 05:57:11 +0900

    1개만 가지고 오고 싶을땡

--- src/main/java/com/zzizily/tech/spring/jpa/member/MemberRepository.java
+++ src/main/java/com/zzizily/tech/spring/jpa/member/MemberRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-  List<Member> findByName(String name);
+  Member findByName(String name);
 
   List<Member> findByAge(Integer age);
 }
